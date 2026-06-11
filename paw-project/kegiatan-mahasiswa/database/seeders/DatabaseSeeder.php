aef030c0036e3b108d52f2d48e61ae37bbfbe145
Dwuds <dawfw7399@gmail.com>
2026-06-11 17:34:17 +0700

    Update DatabaseSeeder.php

--- paw-project/kegiatan-mahasiswa/database/seeders/DatabaseSeeder.php
+++ paw-project/kegiatan-mahasiswa/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password123'),
+            'role' => 'mahasiswa',
         ]);
     }
 }
