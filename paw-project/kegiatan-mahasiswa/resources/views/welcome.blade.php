75179de72d538eefcbb71cb111d9f5d6c7fb037a
luthfisahni <luthfisahni@gmail.com>
2026-06-11 17:18:40 +0700

    favicon

--- paw-project/kegiatan-mahasiswa/resources/views/welcome.blade.php
+++ paw-project/kegiatan-mahasiswa/resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
